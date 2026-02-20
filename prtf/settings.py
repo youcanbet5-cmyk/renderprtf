ce961623923288e5648204bf86353aadb24f5bcc
ToxicNotOp <irfan.thenoor@gmail.com>
2026-02-20 22:48:35 +0530

    changes

--- prtf/settings.py
+++ prtf/settings.py
@@ -5,6 +5,7 @@ import os
 import dj_database_url
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 
@@ -159,7 +160,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'youcanbet5@gmail.com'
-EMAIL_HOST_PASSWORD = 'cgfcotuqzifcdwkd'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
